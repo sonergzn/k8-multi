6f98381af461b22928f5fb1d266c793245cade66
sonergzn <soner.gzn@outlook.com>
2023-04-10 19:04:33 +0200

    testing jpeg

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import videome from './videome.mp4'
+import j from './j.jpeg'
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
@@ -10,13 +10,19 @@ function App() {
   return (
     <Router>
       <div className="App">
-      <video src={videome} 
-      width="600" 
-      height="300" 
-      controls="controls" 
-      autoplay="true" />
+      <header className="App-header"> 
+          <img src={j} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Gecmis olsun :)))
+          </a>
+        </header>
         <div>
-          <p> Gecmis olsun :)))) </p> <br/>
+
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
         </div>
