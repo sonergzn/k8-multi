a99b7530a05a02b3cc452f345681c46245097fe4
sonergzn <soner.gzn@outlook.com>
2023-04-10 03:09:49 +0200

    Some other try

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page!!!!!!!!!!!!!!!
+      
       OtherPage
       <Link to="/">Go back home</Link>
     </div>
