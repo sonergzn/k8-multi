a59a43a90a992dfd64997ca4c8c2e481f4e019b5
sonergzn <soner.gzn@outlook.com>
2023-04-12 18:18:04 +0200

    Small change

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            VERSION K8
+            VERSION K8 - update
           </a>
           <Link to="/">Click-Home</Link> 
           <Link to="/otherpage">Other Page</Link>
