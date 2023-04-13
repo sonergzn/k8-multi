e53905a58d8cddc1d5b1bb645502bac28f62d87d
sonergzn <soner.gzn@outlook.com>
2023-04-14 01:01:32 +0200

    test-5

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            VERSION K8 - argocd - update
+            VERSION K8
           </a>
           <Link to="/">Click-Home</Link> 
           <Link to="/otherpage">Other Page</Link>
