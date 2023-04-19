b6c190779b0160629ae8e79408e209c93c53051f
sonergzn <soner.gzn@outlook.com>
2023-04-19 02:50:55 +0200

    remove one test

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            VERSION K8 - TEST - TEST
+            VERSION K8 - TEST
           </a>
           <Link to="/">Click-Home</Link> 
           <Link to="/otherpage">Other Page</Link>
